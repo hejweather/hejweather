f74a859e42683c395c349a90140b8c369091671c
mattieheden <mattieheden@gmail.com>
2024-10-29 14:16:45 +0100

    Not working to change background based on "weather". Testing background image based on temp. Working. Remake for weather later.

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 10) ? 'app clouds' : 'app') : 'app'}>
       <main>
         <div className="search-box">
           <input 
